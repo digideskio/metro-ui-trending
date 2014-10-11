9a432aea4cae80f95dd0b58281b07d00adcc6e5e
matthumphreys <matt.humphreys@metro.co.uk>
2014-10-11 15:47:22 +0100

    First cut

---
+++ image.rb
@@ -0,0 +1,27 @@
+require 'uri'
+require 'cgi'
+
+# Handles resizing
+class Image
+	attr_reader :url, :width, :height
+
+	def initialize(original_url, width, height)
+		@width = width
+		@height = height
+		uri = URI.parse(original_url)
+		params = CGI.parse(uri.query)
+		crop = params['crop'].first
+		base = "#{uri.scheme}://#{uri.host}#{uri.path}"
+
+		if crop === '1'
+			# Image is auto-cropped (from centre), so we can just specify width and height
+			@url = "#{base}?crop=1&w=#{width}&h=#{height}"
+		elsif !crop.nil?
+			# Manual crop, add resize
+			@url = "#{base}?crop=#{URI.escape(crop)}&resize=#{width}%2C#{height}"
+		else
+			# No crop, add crop=1 for auto-crop
+			@url = "#{base}?crop=1&w=#{width}&h=#{height}"
+		end
+	end
+end
---
+++ main.rb
@@ -0,0 +1,44 @@
+require 'sinatra'
+require 'open-uri'
+require 'json'
+require './image'
+
+API_BASE = 'http://api.metro.co.uk/news-feed/'
+
+def process_fragment()
+	json_str = open(API_BASE) { |io| io.read }
+	data = JSON.parse(json_str)
+	erb :fragment, :locals => {:data => data, :layout => params['layout']}
+end
+
+get '/' do
+	process_fragment()
+end
+
+get '/embedded' do
+    erb :embedded, :locals => {:fragment => process_fragment()}
+end
+
+helpers do
+	# Useful for including inline CSS
+	def get_file_content(path)
+		File.read(File.expand_path(path))
+	end
+
+  	def esc_quotes(str)
+  		return (str.nil?) ? nil : str.sub('"', "&quot;")
+  	end
+
+  	def get_image(post, layout)
+  		url = post['attachments']['landscape_16_9']['URL']
+
+  		case layout
+  		when 'small'
+  			return nil
+  		when 'medium'
+  			return Image.new(url, 229, 305)
+  		else
+  			return Image.new(url, 235, 313)
+  		end
+  	end
+end
